e2e416138cbf817e02d6f5bc88b2b3a701552f13
Scott Serage <xttocsurge51@gmail.com>
2017-09-21 14:45:30 -0600

    Completed Step 7

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
-//SDS I will mark code I have changed from the original document with
-//the letters "SDS".
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const massive = require('massive'); //SDS
-const dotenv = require('dotenv').config(); //SDS dotenv README says to do this as soon as possible.
+const massive = require('massive');
+
+require('dotenv').config();
 
 const app = express();
+massive(process.env.CONNECTION_STRING).then(dbInstance => {
+  app.set('db', dbInstance);
+  // dbInstance.new_planes()
+  //   .then(planes=>console.log(planes))
+  // //   .catch(err=>console.log(err));
+  // dbInstance.get_planes()
+  //   .then(planes=>console.log(planes))
+  //   .catch(err=>console.log(err));
+});
+const controller = require('./controller');
 app.use( bodyParser.json() );
 app.use( cors() );
+
+app.get('/api/planes/', controller.getPlanes);
 
 const port = process.env.PORT || 3000
 app.listen( port , () => { console.log(`Server listening on port ${port}`); } );
